f8422e473817e334d19103f945ecfcbc5625d6ff
Milica Radumilo <radumilomilica@gmail.com>
2022-05-03 19:40:35 +0200

    Add maths class, and maths tests

---
+++ Maths.js
@@ -0,0 +1,11 @@
+class Maths {
+    static add(num1, num2) {
+        return num1 + num2;
+    }
+
+    static sub(num1, num2) {
+        return num1 - num2;
+    }
+}
+
+module.exports = Maths;
---
+++ test/maths.test.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+const MyMath = require('../Maths');
+
+describe('Basic maths test', function() {
+    it('Test if 1 equeals 1', function() {
+        assert.equal(1, 1);
+    });
+
+    it('Test if 1 equals 2', function() {
+        assert.equal(1, 2);
+    });
+});
+
+describe('Maths test', function() {
+    it('Test if 1 + 1 = 2', function() {
+        assert.equal(MyMath.add(1, 1), 2);
+    });
+
+    it('Test if 2 - 1 = 1', function() {
+        assert.equal(MyMath.sub(2, 1), 1);
+    });
+});
