464cab0d11cf7be1521a313a823f4226cf1be70b
Milica Radumilo <radumilomilica@gmail.com>
2022-05-11 16:21:55 +0200

    update

--- test/shop.test.js
+++ test/shop.test.js
@@ -2,7 +2,6 @@ const webdriver = require('selenium-webdriver');
 const { By, Key, until } = require('selenium-webdriver')
 
 const chai = require('chai');
-const { urlContains } = require('selenium-webdriver/lib/until');
 const assert = chai.assert;
 const expect = chai.expect;
 
@@ -17,28 +16,78 @@ describe('shop.qa.rs tests', function() {
         await driver.quit();
     });
 
+    beforeEach(async function() {
+        // Pokrece se pre svakog testa
+    });
+
+    afterEach(async function() {
+        // Pokrece se nakon svakog testa
+    });
+
     it("Verify homepage is open", async function() {
         await driver.get("http://shop.qa.rs/");
 
-        const titleElement = await driver.findElement(By.css(`h1`));
+        const titleElement = await driver.findElement(By.css('h1'));
         const titleContent = await titleElement.getText();
-        expect(titleContent).to.contain(`(QA) Shop`);
-        //isto kao iznad samo krace
-        //expect(await driver.findElement(By.css(`h1`)).getText()).to.contain(`(QA) Shop`);
+        expect(titleContent).to.contain('(QA) Shop');
+        // isto kao i iznad, samo krace
+        //expect(await driver.findElement(By.css('h1')).getText()).to.contain('(QA) Shop');
 
         const divElement = await driver.findElement(By.xpath('//*[@class="row" and contains(., "ORDER YOUR BUGS")]'));
+
         expect(await divElement.isDisplayed()).to.be.true;
-
-  
     });
-     // Domaći
 
-     it("Verify registration page is open", async function() {
-        await driver.get("http://shop.qa.rs/");  
-    
-     const registration = await driver.findElement(By.xpath('//*[@class="col-sm-6 text-center" and contains(., "Register")]'));
-     registration.click();
-     await driver.wait(until.elementLocated(By.name("register")));
-     await driver.sleep(2000);
+    it("Goes to the registration page", async function() {
+        const register = await driver.findElement(By.linkText('Register'));
+        await register.click();
+
+        expect(await driver.findElement(By.name('register')).getAttribute('value')).to.contain('Register');
     });
+
+    it("Successfully performs registration", async function() {
+        const inputIme = await driver.findElement(By.name('ime'));
+        inputIme.sendKeys('Bob');
+
+        const inputPrezime = await driver.findElement(By.name('prezime'));
+        inputPrezime.sendKeys('Buttons');
+
+        const inputEmail = await driver.findElement(By.name('email'));
+        inputEmail.sendKeys('bob.buttons@example.local');
+
+        const inputKorisnicko = await driver.findElement(By.name('korisnicko'));
+        inputKorisnicko.sendKeys('bob.buttons');
+
+        const inputLozinka = await driver.findElement(By.name('lozinka'));
+        inputLozinka.sendKeys('nekaLozinka123');
+
+        const inputLozinkaOpet = await driver.findElement(By.name('lozinkaOpet'));
+        inputLozinkaOpet.sendKeys('nekaLozinka123');
+
+        const buttonRegistracija = await driver.findElement(By.name('register'));
+        await buttonRegistracija.click();
+
+        expect(await driver.findElement(By.className('alert alert-success')).getText()).to.contain('Uspeh!');
+    });
+
+    it("Goes to login page", async function() {
+        const login = await driver.findElement(By.linkText('Login'));
+        await login.click();
+
+        expect(await driver.findElement(By.css('h2')).getText()).to.contain('Prijava');
+    });
+
+    it("Successfully performs login", async function() {
+        const username = await driver.findElement(By.name('username'));
+        username.sendKeys('aaa');
+
+        const password = await driver.findElement(By.name('password'));
+        password.sendKeys('aaa');
+
+        const login = await driver.findElement(By.name('login'));
+        await login.click();
+
+        expect(await driver.findElement(By.css('h2')).getText()).to.contain('Welcome back');
+    });
+
 });
